c502ffe5b4e6c0edf9a0ce2be1e8c742a0f768db
Juan Carlos Del Rio <jcdre@hotmail.com>
2024-04-28 12:16:15 -0500

    Completando la logica a las otras opciones y arreglando la estetica y la despedida

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Principal {
   public static void main(String[] args) {
-    Boolean repetir = true;
+    boolean repetir = true;
     CambioMoneda busqueda;
     Double valor;
 
