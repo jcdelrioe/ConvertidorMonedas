6a23d1cb9c6d7691dc990d2fff0368786dfe8384
Juan Carlos Del Rio <jcdre@hotmail.com>
2024-04-28 11:28:38 -0500

    Probando conversion con USD a ARS

--- src/ConsultaMoneda.java
+++ src/ConsultaMoneda.java
@@ -1,3 +1,27 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ConsultaMoneda {
+  public CambioMoneda buscarCambioMoneda(String base_code, String target_code){
+    URI direccion = URI
+        .create("https://v6.exchangerate-api.com/v6/9bce258dc53e32151b2e03ae/pair/"
+            +base_code+"/"+target_code);
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request = HttpRequest.newBuilder()
+        .uri(direccion)
+        .build();
+
+    try{
+      HttpResponse<String> response = client
+          .send(request, HttpResponse.BodyHandlers.ofString());
+      return new Gson().fromJson(response.body(), CambioMoneda.class);
+    }catch (Exception e){
+      throw new RuntimeException(e.getMessage());
+    }
+  }
 
 }
--- src/Principal.java
+++ src/Principal.java
@@ -4,22 +4,31 @@ public class Principal {
   public static void main(String[] args) {
     Boolean repetir = true;
     System.out.println("Bienvenido al convertidor de monedas");
+    ConsultaMoneda consultaMonedasAConvertir = new ConsultaMoneda();
 
     while (repetir){
-      System.out.println("1) Dolar Americano (USD) -> Peso Argentino (ARG)");
-      System.out.println("2) Peso Argentino (ARG) -> Dolar Americano (USD)");
+      System.out.println("1) Dolar Americano (USD) -> Peso Argentino (ARS)");
+      System.out.println("2) Peso Argentino (ARS) -> Dolar Americano (USD)");
       System.out.println("3) Dolar Americano (USD) -> Real Brasileño (BRL)");
       System.out.println("4) Real Brasileño (BRL) -> Dolar Americano (USD)");
       System.out.println("5) Dolar Americano (USD) -> Peso Colombiano (COP)");
       System.out.println("6) Peso Colombiano (COP) -> Dolar Americano (USD)");
       System.out.println("7) Salir del Programa");
-      System.out.println("Ingrese una opcion");
+      System.out.println("Ingrese una opcion ");
 
       Scanner scanner = new Scanner(System.in);
       String opcion = scanner.nextLine();
       switch (opcion){
         case "1":
-          System.out.println("Opcion 1");
+          System.out.println("=================================");
+          System.out.println("Convertir (USD) a (ARG)");
+          System.out.println("Ingrese la cantidad de USD a convertir: ");
+          Double valor = scanner.nextDouble();
+
+          CambioMoneda busqueda = consultaMonedasAConvertir.buscarCambioMoneda("USD", "ARS");
+
+          System.out.println(valor + " " + busqueda.base_code() + " son " + valor * busqueda.conversion_rate() + " " + busqueda.target_code());
+          System.out.println("=================================");
           break;
         case "2":
           System.out.println("Opcion 2");
